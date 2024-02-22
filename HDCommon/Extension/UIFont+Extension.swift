0774bbffdb7c55bc26d52ce7937c96c724514624
hendy <309875551@qq.com>
2024-02-22 11:17:26 +0800

    常用Extension

---
+++ HDCommon/Extension/UIFont+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  UIFont+Extension.swift
+//
+//
+//  Created by bailun on 2024/2/22.
+//
+
+public extension UIFont {
+    
+    /// 细体
+    static func thin(of size: CGFloat, name: String = "PingFangSC-Thin") -> UIFont {
+        return UIFont(name: name, size: size) ?? UIFont.systemFont(ofSize: size, weight: .thin)
+    }
+    
+    /// 常规
+    static func regular(of size: CGFloat, name: String = "PingFangSC-Regular") -> UIFont {
+        return UIFont(name: name, size: size) ?? UIFont.systemFont(ofSize: size, weight: .regular)
+    }
+    
+    /// 中等
+    static func medium(of size: CGFloat, name: String = "PingFangSC-Medium") -> UIFont {
+        return UIFont(name: name, size: size) ?? UIFont.systemFont(ofSize: size, weight: .medium)
+    }
+    
+    /// 粗体
+    static func bold(of size: CGFloat, name: String = "PingFangSC-Semibold") -> UIFont {
+        return UIFont(name: name, size: size) ?? UIFont.systemFont(ofSize: size, weight: .bold)
+    }
+}
+
